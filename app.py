7e5c1a5cc8483fbc6d11ddf1bfd652793653c9b4
parsTroy <troyparsons@hotmail.com>
2022-10-23 16:49:05 -0400

    comparison operators

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-temperature = 15
-if temperature > 30:
-    print("It's warm")
-    print("Drink water")
-elif temperature > 20:
-    print("It's nice")
+# temperature = 15
+# if temperature > 30:
+#     print("It's warm")
+#     print("Drink water")
+# elif temperature > 20:
+#     print("It's nice")
+# else:
+#     print("It's cold")
+# print("Done")
+
+
+# age = 12
+# if age >= 18:
+#     message = "Eligible"
+# else:
+#     message = "Not eligible"
+
+# message = "Eligible" if age >= 18 else "Not eligible"
+# print(message)
+
+
+high_income = True
+good_credit = True
+student = False
+
+if (high_income or good_credit) and not student:
+    print("Eligible")
 else:
-    print("It's cold")
-print("Done")
+    print("Not eligible")

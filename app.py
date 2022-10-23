4dba6eb83a99f57f9ddd430fee6ad94285797a8d
parsTroy <troyparsons@hotmail.com>
2022-10-23 16:41:25 -0400

    if else if statements

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-import math
-
-x = input('x: ')
-y = x + 1
+temperature = 15
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done")

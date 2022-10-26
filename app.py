a4b9dee66c6dbe7bd34ad75d56b8699582cf3d27
parsTroy <troyparsons@hotmail.com>
2022-10-25 21:16:49 -0400

    uupdate code

--- app.py
+++ app.py
@@ -19,11 +19,11 @@
 # print(message)
 
 
-high_income = True
-good_credit = True
-student = False
+# high_income = True
+# good_credit = True
+# student = False
 
-if (high_income or good_credit) and not student:
-    print("Eligible")
-else:
-    print("Not eligible")
+# if (high_income or good_credit) and not student:
+#     print("Eligible")
+# else:
+#     print("Not eligible")

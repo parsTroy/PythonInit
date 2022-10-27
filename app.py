470bf1e2c98baae8e41157cb1951280b84cfb95d
parsTroy <troyparsons@hotmail.com>
2022-10-26 20:15:10 -0400

    update loops

--- app.py
+++ app.py
@@ -27,3 +27,44 @@
 #     print("Eligible")
 # else:
 #     print("Not eligible")
+
+
+# successful = False
+# for number in range(3):
+#     print('Attempt')
+#     if successful:
+#         print('Successful')
+#         break
+# else:
+#     print('Attempted 3 times and failed')
+
+
+# for x in range(5):
+#     for y in range(3):
+#         print(f'({x}, {y})')
+
+# print(type(5))
+# print(type(range(5)))
+
+# # Iterable
+# for item in shopping_cart:
+#     print(item)
+
+# number = 100
+# while number > 0:
+#     print(number)
+#     number //= 2
+
+# while True:
+#     command = input('>')
+#     print('ECHO', command)
+#     if command.lower() == 'quit':
+#         break
+
+count = 0
+for x in range(1, 10):
+    if x % 2 == 0:
+        count += 1
+        print(x)
+
+print(f'We have {count} even numbers')
